4a2fe142127893249e170220f7954a9c551eee12
akifbsr <113089623+akifbsr@users.noreply.github.com>
2023-04-19 02:19:48 +0300

    Ödev2 oluşturuldu

---
+++ script.js
@@ -0,0 +1,22 @@
+const stil = () => {
+    const body = document.getElementById("body");
+    body.classList.add("body");
+    
+    const section = document.getElementById("section");
+    section.classList.add("section");
+   
+    const title = document.getElementById("title");
+    title.classList.add("title");
+
+    const form = document.getElementById("form");
+    form.classList.add("form");
+    
+    const button = document.getElementById("button");
+    button.classList.add("button");
+
+    const buton = document.getElementById("buton");
+    buton.classList.add("buton");
+
+  }
+
+ 
